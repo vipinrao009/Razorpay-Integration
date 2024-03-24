f422f5983d994107024136fe27bea9e92c8b0a11
vipinrao009 <vipinrao2018@gmail.com>
2024-03-24 15:56:30 +0530

    move on frontend

--- server/app.js
+++ server/app.js
@@ -2,17 +2,20 @@ import { config } from "dotenv";
 config({path:"./config/config.env"})
 import  express from "express";
 import Razorpay from "razorpay"
+import cors from "cors"
 import paymentRouter from "./routes/paymentRouter.js";
 
 
-const app = express();
 
+const app = express();
+app.use('/api',paymentRouter)
+app.use(cors())
 
 export const instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_SECRET_KEY,
   });
-app.use('/api',paymentRouter)
+
 
 
 
